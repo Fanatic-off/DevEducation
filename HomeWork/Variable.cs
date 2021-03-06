f1453f280f14b9c2480109c1d57b2eb7f6a73691
Eugene <Zhekul.90@gmail.com>
2021-03-06 20:20:00 +0300

    done Veriable task 4

--- HomeWork/Variable.cs
+++ HomeWork/Variable.cs
@@ -6,18 +6,18 @@ public class Variable
 	{
 		static void Main(string[] args)
         {
-			//Пользователь вводит 2 числа(A и B).Выведите в консоль решение (5 * A + B*B)/(B - A)
-			//Console.WriteLine("Введите первое целое число");
-			//int A = Convert.ToInt32(Console.ReadLine());
+            //Пользователь вводит 2 числа(A и B).Выведите в консоль решение (5 * A + B*B)/(B - A)
+            Console.WriteLine("Введите первое целое число");
+            int A = Convert.ToInt32(Console.ReadLine());
 
-			//Console.WriteLine("Введите второе целое число");
-			//int B = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе целое число");
+            int B = Convert.ToInt32(Console.ReadLine());
 
-			//int result = (5 * A + B * B) / (B - A);
-			//Console.WriteLine($"{result}");
+            int result = (5 * A + B * B) / (B - A);
+            Console.WriteLine($"{result}");
 
-			//Пользователь вводит 2 значения(A и B).Поменяйте содержимое переменных A и B местами.
-			Console.WriteLine("Введите целое число A");
+            //Пользователь вводит 2 значения(A и B).Поменяйте содержимое переменных A и B местами.
+            Console.WriteLine("Введите целое число A");
 			int A2 = Convert.ToInt32(Console.ReadLine());
 
 			Console.WriteLine("Введите второе целое число B");
@@ -37,7 +37,7 @@ public class Variable
 			Console.WriteLine($"результат деления -{A3 / B3}");
 			Console.WriteLine($"остаток от деления - {A3 % B3}");
 
-			//Пользователь вводит 3 числа(A, B и С).Выведите в консоль решение(значение X) линейного уравнения стандартного вида, где A* X+B = C.
+			//Пользователь вводит 3 числа(A, B и С).Выведите в консоль решение(значение X) линейного уравнения стандартного вида, где A*X + B = C.
 			Console.WriteLine("Введите целое число A");
 			int A4 = Convert.ToInt32(Console.ReadLine());
 
@@ -47,7 +47,8 @@ public class Variable
 			Console.WriteLine("Введите третье целое число C");
 			int C4 = Convert.ToInt32(Console.ReadLine());
 
-
+			int result4 = (C4 - B4) / A4;
+			Console.WriteLine($"{result4}");
 
 			//Пользователь вводит 4 числа(X1, Y1, X2, Y2), описывающие координаты 2 - х точек на координатной плоскости.
 			//	Выведите уравнение прямой в формате Y = AX + B, проходящей через эти точки.
