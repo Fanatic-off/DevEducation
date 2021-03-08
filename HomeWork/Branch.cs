405f902ce6894573fce94e000573ed486f2622f1
Eugene <Zhekul.90@gmail.com>
2021-03-08 13:00:08 +0300

    done Branch task 5

--- HomeWork/Branch.cs
+++ HomeWork/Branch.cs
@@ -157,7 +157,214 @@ namespace HomeWork
             }
 
             //Пользователь вводит двузначное число. Выведите в консоль прописную запись этого числа.
-            //                Например при вводе “25” в консоль будет выведено “двадцать пять”.
+            //Например при вводе “25” в консоль будет выведено “двадцать пять”.
+            Console.WriteLine("Введите двузначное число");
+            int A5 = Convert.ToInt32(Console.ReadLine());
+
+            //через switch
+            switch (A5)
+            {
+                case 1:
+                    Console.WriteLine("один");
+                    break;
+                case 2:
+                    Console.WriteLine("два");
+                    break;
+                case 3:
+                    Console.WriteLine("три");
+                    break;
+                case 4:
+                    Console.WriteLine("четыре");
+                    break;
+                case 5:
+                    Console.WriteLine("пять");
+                    break;
+                case 6:
+                    Console.WriteLine("шесть");
+                    break;
+                case 7:
+                    Console.WriteLine("семь");
+                    break;
+                case 8:
+                    Console.WriteLine("восемь");
+                    break;
+                case 9:
+                    Console.WriteLine("девять");
+                    break;
+                case 10:
+                    Console.WriteLine("десять");
+                    break;
+                case 11:
+                    Console.WriteLine("одиннадцать");
+                    break;
+            }
+            //через if
+            if (A5 < 10)
+            {
+                if (A5 == 1)
+                {
+                    Console.WriteLine("один");
+                }
+                else if (A5 == 2)
+                {
+                    Console.WriteLine("два");
+                }
+                else if (A5 == 3)
+                {
+                    Console.WriteLine("три");
+                }
+                else if (A5 == 4)
+                {
+                    Console.WriteLine("четыре");
+                }
+                else if (A5 == 5)
+                {
+                    Console.WriteLine("пять");
+                }
+                else if (A5 == 6)
+                {
+                    Console.WriteLine("шесть");
+                }
+                else if (A5 == 7)
+                {
+                    Console.WriteLine("семь");
+                }
+                else if (A5 == 8)
+                {
+                    Console.WriteLine("восемь");
+                }
+                else if (A5 == 9)
+                {
+                    Console.WriteLine("девять");
+                }
+
+            }
+            else if (10 <= A5 && A5 < 20)
+            {
+                if (A5 % 10 == 0)
+                {
+                    Console.WriteLine("десять");
+                }
+                else if (A5 % 10 == 1)
+                {
+                    Console.WriteLine("одиннадцать");
+                }
+                else if (A5 % 10 == 2)
+                {
+                    Console.WriteLine("двенадцать");
+                }
+                else if (A5 % 10 == 3)
+                {
+                    Console.WriteLine("тринадцать");
+                }
+                else if (A5 % 10 == 4)
+                {
+                    Console.WriteLine("четырнадцать");
+                }
+                else if (A5 % 10 == 5)
+                {
+                    Console.WriteLine("пятьнадцать");
+                }
+                else if (A5 % 10 == 6)
+                {
+                    Console.WriteLine("шестьнадцать");
+                }
+                else if (A5 % 10 == 7)
+                {
+                    Console.WriteLine("семьнадцать");
+                }
+                else if (A5 % 10 == 8)
+                {
+                    Console.WriteLine("восемьнадцать");
+                }
+                else if (A5 % 10 == 9)
+                {
+                    Console.WriteLine("девятьнадцать");
+                }
+            }
+            else if (20 <= A5 && A5 < 30)
+            {
+
+                Console.WriteLine("двадцать ");
+                if (A5 % 10 == 1)
+                {
+                    Console.Write("один");
+                }
+                else if (A5 % 10 == 2)
+                {
+                    Console.Write("два");
+                }
+                else if (A5 % 10 == 3)
+                {
+                    Console.Write("три");
+                }
+                else if (A5 % 10 == 4)
+                {
+                    Console.Write("четыре");
+                }
+                else if (A5 % 10 == 5)
+                {
+                    Console.Write("пять");
+                }
+                else if (A5 % 10 == 6)
+                {
+                    Console.Write("шесть");
+                }
+                else if (A5 % 10 == 7)
+                {
+                    Console.Write("семь");
+                }
+                else if (A5 % 10 == 8)
+                {
+                    Console.Write("восемь");
+                }
+                else if (A5 % 10 == 9)
+                {
+                    Console.Write("девять");
+                }
+
+            }
+            else if (30 <= A5 && A5 < 40)
+            {
+                Console.WriteLine("тридцать ");
+                if (A5 % 10 == 1)
+                {
+                    Console.Write("один");
+                }
+                else if (A5 % 10 == 2)
+                {
+                    Console.Write("два");
+                }
+                else if (A5 % 10 == 3)
+                {
+                    Console.Write("три");
+                }
+                else if (A5 % 10 == 4)
+                {
+                    Console.Write("четыре");
+                }
+                else if (A5 % 10 == 5)
+                {
+                    Console.Write("пять");
+                }
+                else if (A5 % 10 == 6)
+                {
+                    Console.Write("шесть");
+                }
+                else if (A5 % 10 == 7)
+                {
+                    Console.Write("семь");
+                }
+                else if (A5 % 10 == 8)
+                {
+                    Console.Write("восемь");
+                }
+                else if (A5 % 10 == 9)
+                {
+                    Console.Write("девять");
+                }
+            }
+
 
         }
     }
