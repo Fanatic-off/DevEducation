93d53b697302655d62971dddff00ced35f1807b7
Eugene <Zhekul.90@gmail.com>
2021-03-09 19:04:31 +0300

    done random in ArrayTwoDimensional

--- HomeWork/ArrayTwoDimensional.cs
+++ HomeWork/ArrayTwoDimensional.cs
@@ -6,5 +6,22 @@ namespace HomeWork
 {
     class ArrayTwoDimensional
     {
+        public static void Main(String[] args)
+        {
+            Random rnd = new Random();
+            int[,] array = new int[5, 5];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    int varRandom = rnd.Next(0, 99);
+                    array[i, j] = varRandom;
+                    Console.Write($"{array[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
     }
 }
