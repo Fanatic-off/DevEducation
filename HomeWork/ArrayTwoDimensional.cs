a69921cf5a07abbdb928890b0c63992afac7616f
Eugene <Zhekul.90@gmail.com>
2021-03-09 20:08:28 +0300

    done ArrayTwoDimensional task 4

--- HomeWork/ArrayTwoDimensional.cs
+++ HomeWork/ArrayTwoDimensional.cs
@@ -65,6 +65,21 @@ namespace HomeWork
             Console.WriteLine($"индекс минимального элемента массива - {minIndexI},{minIndexJ} ");
 
             //Найти индекс максимального элемента массива
+            int maxIndexI = 0;
+            int maxIndexJ = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] >= max)
+                    {
+                        maxIndexI = i;
+                        maxIndexJ = j;
+                    }
+                }
+            }
+            Console.WriteLine($"индекс максимального элемента массива - {maxIndexI},{maxIndexJ}");
+
             //Найти количество элементов массива, которые больше всех своих соседей одновременно
             //Отразите массив относительно его главной диагонали
 
