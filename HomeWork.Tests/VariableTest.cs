1433d3b0267e04257a02be8c22aa2799f4844456
Eugene <Zhekul.90@gmail.com>
2021-03-18 16:22:57 +0300

    add Variable Tests

--- HomeWork.Tests/VariableTest.cs
+++ HomeWork.Tests/VariableTest.cs
@@ -5,9 +5,9 @@ namespace HomeWork.Tests
     public class Tests
     {
 
-        [TestCase(1,2,9)]
-        [TestCase(0,1,1)]
-        [TestCase(5,6,61)]
+        [TestCase(1, 2, 9)]
+        [TestCase(0, 1, 1)]
+        [TestCase(5, 6, 61)]
         public void PrintSolvingTest(int a, int b, int expected)
         {
             int actual = (5 * a + b * b) / (b - a);
@@ -15,10 +15,50 @@ namespace HomeWork.Tests
             Assert.AreEqual(expected, actual);
         }
 
-        public void ChangeVariableTest(int a, int b, int expected)
+        [TestCase(2, 1, true)]
+        [TestCase(4, 10, true)]
+        [TestCase(-5, 20, true)]
+        public void ChangeVariableTest(int a, int b, bool expected)
         {
+            bool actual = false;
+
+            int tmp1 = a;
+            int tmp2 = b;
             
-            
+            int tmp = a;
+            a = b;
+            b = tmp;
+
+            if ((tmp1 == b) && (tmp2 == a))
+            {
+                actual = true;
+            }
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(1, 2, 3, 1)]
+        [TestCase(2, 4, 7, 1)]
+        [TestCase(3, 22, 55, 11)]
+        public void PrintSolvingLinearEquationTest(int a, int b, int c, int expected)
+        {
+            int actual = (c - b) / a;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(2, 3, 1, 2, 1, 1, true)]
+        [TestCase(5, 5, 1, 2, 0.75, 1.25, true)]
+        [TestCase(7, 2, 3, 1, 0.25, 0.25, true)]
+        public void PrintEquationLineOverTwoPointsTest(double x1, double y1, double x2, double y2, double A, double B, bool expected)
+        {
+            bool actual = false;
+
+            if (((y1 - y2) / (x1 - x2) == A) && ((y2 - ((y1 - y2) / (x1 - x2)) * x2) == B))
+            {
+                actual = true;
+            }
+
             Assert.AreEqual(expected, actual);
         }
     }
