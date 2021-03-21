a27417f114c50cb8b4473f5d323f586b8858ad84
Eugene <Zhekul.90@gmail.com>
2021-03-22 00:03:11 +0300

    add ArrayOneDimensionalTests FindMinElementTest

---
+++ HomeWork.Tests/ArrayOneDimensionalTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace HomeWork.Tests
+{
+    public class ArrayOneDimensionalTests
+    {
+        [TestCase()]
+        public void FindMinElementTest(int[] a, int expected)
+        {
+            int actual = ArrayOneDimensional.FindMinElement(a);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
